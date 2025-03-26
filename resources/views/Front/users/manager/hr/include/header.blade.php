7669b8ff53b80e500e388139018b83630ba86989
Blaise202 <iblaise2023@gmail.com>
2025-03-26 09:26:40 +0000

    removed unnecessary html

--- resources/views/Front/users/manager/hr/include/header.blade.php
+++ resources/views/Front/users/manager/hr/include/header.blade.php
@@ -2,11 +2,11 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Blaise Motors</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('hr/assets/images/logos/favicon.png') }}" />
-    <link rel="stylesheet" href="{{ asset('hr/assets/css/styles.min.css') }}" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Blaise Motors</title>
+  <link rel="shortcut icon" type="image/png" href="{{ asset('hr/assets/images/logos/favicon.png') }}" />
+  <link rel="stylesheet" href="{{ asset('hr/assets/css/styles.min.css') }}" />
 </head>
 
 <body>
